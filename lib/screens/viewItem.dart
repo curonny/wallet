02f82749d6a97763906a1e77ec7ca8576117a07f
Ronny Montano <rmontano1992@gmail.com>
2022-09-12 06:47:45 -0400

    fix bug

--- lib/screens/viewItem.dart
+++ lib/screens/viewItem.dart
@@ -21,40 +21,20 @@ class _ViewDocumentState extends State<ViewDocument> {
       appBar: AppBar(title: Text(document.nombre.toString())),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          Text(
-            "Registro creado el: ".tr,
-            style: Get.theme.textTheme.bodyText2,
-          ),
-          Text(DateFormat.yMMMd()
-              .format(DateTime.parse(document.date.toString()))),
-          InkWell(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Visibility(
-                  visible: document.imageFront.toString().isNotEmpty,
-                  child: Image.file(File(document.imageFront.toString()))),
+        child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Text(
+              "Registro creado el: ".tr,
+              style: Get.theme.textTheme.bodyText2,
             ),
-            onTap: () {
-              SwipeImageGallery(
-                initialIndex: 0,
-                backgroundColor: Colors.black,
-                context: context,
-                itemBuilder: (context, index) {
-                  return Image.file(
-                    File(document.imageFront.toString()),
-                  );
-                },
-                itemCount: 1,
-              ).show();
-            },
-          ),
-          InkWell(
+            Text(DateFormat.yMMMd()
+                .format(DateTime.parse(document.date.toString()))),
+            InkWell(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Visibility(
-                    visible: document.imageBack.toString().isNotEmpty,
-                    child: Image.file(File(document.imageBack.toString()))),
+                    visible: document.imageFront.toString().isNotEmpty,
+                    child: Image.file(File(document.imageFront.toString()))),
               ),
               onTap: () {
                 SwipeImageGallery(
@@ -68,8 +48,30 @@ class _ViewDocumentState extends State<ViewDocument> {
                   },
                   itemCount: 1,
                 ).show();
-              })
-        ]),
+              },
+            ),
+            InkWell(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Visibility(
+                      visible: document.imageBack.toString().isNotEmpty,
+                      child: Image.file(File(document.imageBack.toString()))),
+                ),
+                onTap: () {
+                  SwipeImageGallery(
+                    initialIndex: 0,
+                    backgroundColor: Colors.black,
+                    context: context,
+                    itemBuilder: (context, index) {
+                      return Image.file(
+                        File(document.imageBack.toString()),
+                      );
+                    },
+                    itemCount: 1,
+                  ).show();
+                })
+          ]),
+        ),
       ),
     );
   }
